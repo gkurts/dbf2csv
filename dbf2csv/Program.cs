e94565fdb1aaeeff9c6b078a47da3ce0ea76f489
Greg Kurts <gkurts@gregkurts.com>
2014-08-14 12:37:07 -0500

    initial commit

---
+++ dbf2csv/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbf2csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(".", "*.dbf");
+            foreach (string file in files)
+            {
+                Console.Write("Processing " + file + "...");
+                using (OleDbConnection connection = new OleDbConnection("Provider=VFPOLEDB.1;Data Source=" + file))
+                {
+                    connection.Open();
+                    DataTable tables = connection.GetSchema(System.Data.OleDb.OleDbMetaDataCollectionNames.Tables);
+
+                    OleDbCommand cmd = new OleDbCommand("select * from " + file, connection);
+                    DataTable dt = new DataTable();
+                    OleDbDataAdapter DA = new OleDbDataAdapter(cmd);
+                    DA.Fill(dt);
+
+                    StringBuilder sb = new StringBuilder();
+
+                    IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                        Select(column => column.ColumnName);
+                    sb.AppendLine(string.Join(",", columnNames));
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+                        sb.AppendLine(string.Join(",", fields));
+                    }
+
+                    File.WriteAllText(Path.ChangeExtension(file, "csv"), sb.ToString());
+                    Console.WriteLine("   - wrote " +dt.Rows.Count+ " lines");
+                }
+            }
+
+            Console.WriteLine("Done!!! Hope it worked!");
+            Console.ReadLine();
+        }
+    }
+}
